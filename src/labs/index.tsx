c22ca2e74faecf41ac3851eead338d82bdce82b4
technophilist <54663474+technophilist@users.noreply.github.com>
2024-09-17 12:10:15 -0400

    fix incorrect paths in Labs component

--- src/labs/index.tsx
+++ src/labs/index.tsx
@@ -13,8 +13,8 @@ function Labs() {
             <Routes>
                 <Route path="/" element={<Navigate to="lab1"/>}/>
                 <Route path="lab1" element={<Lab1/>}/>
-                <Route path="lab1" element={<Lab2/>}/>
-                <Route path="lab1" element={<Lab3/>}/>
+                <Route path="lab2" element={<Lab2/>}/>
+                <Route path="lab3" element={<Lab3/>}/>
             </Routes>
         </div>
     )
