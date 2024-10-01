ed1f3818032b8abae811ba4c09b5d622f3a9b843
technophilist <54663474+technophilist@users.noreply.github.com>
2024-10-01 12:30:40 -0400

    style course navigation sidebar (4.3)

--- src/kanbas/Dashboard.tsx
+++ src/kanbas/Dashboard.tsx
@@ -1,70 +1,88 @@
 import {Link} from "react-router-dom";
 import courseImage from "../images/reactjs.jpeg"
-import {useMemo} from "react";
+import React, {useMemo} from "react";
+import {SlNote} from "react-icons/sl";
+import {FaEdit} from "react-icons/fa";
 
 const courseList = [
     {
-        dashboardTitle: "CS1234 React JS",
-        courseTitle: "Full Stack software developer",
+        id: "1",
+        title: "CS4550 12631 Web Development",
+        subtitle: "CS4550.12631.202410",
+        yearAndSection: "202410_1 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
         route: "/kanbas/courses/1234/home"
     },
     {
-        dashboardTitle: "CS5678 Node JS",
-        courseTitle: "Backend Development",
+        id: "2",
+        title: "CS3200 12632 Database Design",
+        subtitle: "CS3200.12632.202410",
+        yearAndSection: "202410_2 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/5678/home"
+        route: "/kanbas/courses/1235/home"
     },
     {
-        dashboardTitle: "CS9101 Python",
-        courseTitle: "Data Science",
+        id: "3",
+        title: "CS4500 12633 Software Development",
+        subtitle: "CS4500.12633.202410",
+        yearAndSection: "202410_3 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/9101/home"
+        route: "/kanbas/courses/1236/home"
     },
     {
-        dashboardTitle: "CS1121 Java",
-        courseTitle: "Enterprise Applications",
+        id: "4",
+        title: "CS5600 12634 Computer Systems",
+        subtitle: "CS5600.12634.202410",
+        yearAndSection: "202410_4 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/1121/home"
+        route: "/kanbas/courses/1237/home"
     },
     {
-        dashboardTitle: "CS3141 C++",
-        courseTitle: "System Programming",
+        id: "5",
+        title: "CS5800 12635 Algorithms",
+        subtitle: "CS5800.12635.202410",
+        yearAndSection: "202410_5 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/3141/home"
+        route: "/kanbas/courses/1238/home"
     },
     {
-        dashboardTitle: "CS5161 Ruby",
-        courseTitle: "Web Development",
+        id: "6",
+        title: "CS6200 12636 Information Retrieval",
+        subtitle: "CS6200.12636.202410",
+        yearAndSection: "202410_6 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/5161/home"
+        route: "/kanbas/courses/1239/home"
     },
     {
-        dashboardTitle: "CS7181 Go",
-        courseTitle: "Concurrent Programming",
+        id: "7",
+        title: "CS6650 12637 Distributed Systems",
+        subtitle: "CS6650.12637.202410",
+        yearAndSection: "202410_7 Fall 2023 Semester Full Term",
         imgSrc: courseImage,
-        route: "/kanbas/courses/7181/home"
-    },
-    {
-        dashboardTitle: "CS9201 Swift",
-        courseTitle: "iOS Development",
-        imgSrc: courseImage,
-        route: "/kanbas/courses/9201/home"
+        route: "/kanbas/courses/1240/home"
     }
 ]
-
 
 function Dashboard() {
     const courses = useMemo(() => {
         return courseList.map((course) => {
             return (
-                <div className="wd-dashboard-course" key={course.courseTitle + course.dashboardTitle}>
-                    <img src={courseImage} width={200}/>
-                    <div>
-                        <Link className="wd-dashboard-course-link"
-                              to={course.route}>{course.dashboardTitle}</Link>
-                        <p className="wd-dashboard-course-title">{course.courseTitle}</p>
-                        <Link to={course.route}>Go</Link>
+                <div className="wd-dashboard-course col"
+                     style={{width: "300px"}}
+                     key={course.id}>
+                    <div className="card rounded-3 overflow-hidden">
+                        <Link className="wd-dashboard-course-link text-decoration-none text-dark"
+                              to={course.route}>
+                            <img src={courseImage} width="100%" height={160}/>
+                            <div className="card-body">
+                                <h6 className="wd-dashboard-course-title card-title text-truncate fw-semibold m-0"
+                                    style={{color: "#0076B8"}}>{course.title}</h6>
+                                <h6 className="wd-dashboard-course-title card-text text-secondary m-0">{course.subtitle}</h6>
+                                <h6 className="text-truncate text-secondary">{course.yearAndSection}</h6>
+                                {/*<button className="btn btn-primary"> </button>*/}
+                                <SlNote/>
+                            </div>
+                        </Link>
                     </div>
                 </div>
             )
@@ -77,7 +95,11 @@ function Dashboard() {
             <hr/>
             <h2 id="wd-dashboard-published">Published Courses (12)</h2>
             <hr/>
-            <div id="wd-dashboard-courses">{courses}</div>
+            <div id="wd-dashboard-courses" className="row">
+                <div className="row row-cols-1 row-cols-md-5 g-4">
+                    {courses}
+                </div>
+            </div>
         </div>
     );
 }
--- src/kanbas/courses/Navigation.tsx
+++ src/kanbas/courses/Navigation.tsx
@@ -2,18 +2,31 @@ import {Link} from "react-router-dom";
 
 function CoursesNavigation() {
     return (
-        <div id="wd-courses-navigation">
-            <Link id="wd-course-home-link" to="/kanbas/courses/1234/home">Home</Link><br/>
-            <Link id="wd-course-modules-link" to="/kanbas/courses/1234/modules">Modules
-            </Link><br/>
-            <Link id="wd-course-piazza-link" to="/kanbas/courses/1234/piazza">Piazza</Link><br/>
-            <Link id="wd-course-zoom-link" to="/kanbas/courses/1234/zoom">Zoom</Link><br/>
-            <Link id="wd-course-quizzes-link" to="/kanbas/courses/1234/assignments">
-                Assignments</Link><br/>
-            <Link id="wd-course-assignments-link" to="/kanbas/courses/1234/quizzes">Quizzes
-            </Link><br/>
-            <Link id="wd-course-grades-link" to="/kanbas/courses/1234/grades">Grades</Link><br/>
-            <Link id="wd-course-people-link" to="/kanbas/people">People</Link><br/>
+        <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+            <Link id="wd-course-home-link"
+                  to="/kanbas/courses/1234/home"
+                  className="list-group-item active border-0">Home</Link>
+            <Link
+                id="wd-course-modules-link"
+                className="list-group-item text-danger border-0"
+                to="/kanbas/courses/1234/modules">Modules</Link>
+            <Link id="wd-course-piazza-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/courses/1234/piazza">Piazza</Link>
+
+            <Link id="wd-course-zoom-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/courses/1234/zoom">Zoom</Link>
+
+            <Link id="wd-course-quizzes-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/courses/1234/assignments">Assignments</Link>
+
+            <Link id="wd-course-assignments-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/courses/1234/quizzes">Quizzes</Link>
+
+            <Link id="wd-course-grades-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/courses/1234/grades">Grades</Link>
+
+            <Link id="wd-course-people-link" className="list-group-item text-danger border-0"
+                  to="/kanbas/people">People</Link>
         </div>);
 }
 
