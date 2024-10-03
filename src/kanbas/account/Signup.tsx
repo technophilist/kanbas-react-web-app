6ab7d75ab492a5fe3b597e82af7c2d8944f335dd
technophilist <54663474+technophilist@users.noreply.github.com>
2024-10-03 19:56:23 -0400

    add styling to Signup component

--- src/kanbas/account/Signup.tsx
+++ src/kanbas/account/Signup.tsx
@@ -5,10 +5,10 @@ function Signup() {
     return (
         <div id="wd-signup-screen">
             <h3>Sign up</h3>
-            <input placeholder="username"/><br/>
-            <input placeholder="password" type="password"/><br/>
-            <input placeholder="verify password" type="password"/><br/>
-            <Link to="/kanbas/account/profile"> Sign up </Link><br/>
+            <input placeholder="username" className="form-control mb-2"/>
+            <input placeholder="password" type="password" className="form-control mb-2"/>
+            <input placeholder="verify password" type="password" className="form-control mb-2"/><br/>
+            <Link to="/kanbas/account/profile"  className="btn btn-primary w-100"> Sign up </Link><br/>
             <Link to="/kanbas/account/signin">Sign in</Link>
         </div>
     );
