deb1943323ab3a4ca8ffe72512afd571ff67eacd
technophilist <54663474+technophilist@users.noreply.github.com>
2024-10-03 20:09:13 -0400

    add styling to account navigation component

--- src/kanbas/account/navigation.tsx
+++ src/kanbas/account/navigation.tsx
@@ -1,11 +1,30 @@
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import {useMemo} from "react";
+
+const navLinkItems = [
+    {link: "/kanbas/account/signin", label: "Signin"},
+    {link: "/kanbas/account/signup", label: "Signup"},
+    {link: "/kanbas/account/profile", label: "Profile"},
+];
 
 function AccountNavigation() {
+    const {pathname} = useLocation()
+    const navigationItems = useMemo(() => {
+
+        return navLinkItems.map((navItem) => {
+            console.log(`${pathname}, ${navItem.link}, ${pathname.includes(navItem.link)}`)
+            return (
+                <Link
+                    key={navItem.link}
+                    to={navItem.link}
+                    className={`list-group-item border-0 ${pathname.includes(navItem.link) ? "active" : "text-danger"}`}> {navItem.label}
+                </Link>
+            )
+        })
+    }, [pathname])
     return (
-        <div id="wd-account-navigation">
-            <Link to={`/kanbas/account/signin`}> Signin </Link> <br/>
-            <Link to={`/kanbas/account/signup`}> Signup </Link> <br/>
-            <Link to={`/kanbas/account/profile`}> Profile </Link> <br/>
+        <div id="wd-account-navigation" className="wd-course-home-link list-group fs-5 rounded-0 wd">
+            {navigationItems}
         </div>
     );
 }
