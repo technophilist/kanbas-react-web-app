7809dce6a8e8b2a7aad7bcb10d25059187a0ac34
technophilist <54663474+technophilist@users.noreply.github.com>
2024-10-24 12:30:07 -0400

    add lab 4 to TOC component

--- src/labs/TOC.tsx
+++ src/labs/TOC.tsx
@@ -9,6 +9,7 @@ function TOC() {
             <li className = "nav-item"><Link to="/labs/lab1" className = {`nav-link ${pathname.includes("lab1") ? "active" : ""}`}>Lab 1</Link></li>
             <li className = "nav-item"><Link to="/labs/lab2" className = {`nav-link ${pathname.includes("lab2") ? "active" : ""}`}>Lab 2</Link></li>
             <li className = "nav-item"><Link to="/labs/lab3" className = {`nav-link ${pathname.includes("lab3") ? "active" : ""}`}>Lab 3</Link></li>
+            <li className = "nav-item"><Link to = "/labs/lab4" className = {`nav-link ${pathname.includes("lab4") ? "active" : ""}`}>Lab 4</Link></li>
             <li className = "nav-item"><Link to="/kanbas" className="nav-link">Kanbas</Link></li>
             <li className = "nav-item"><a className="nav-link" id="wd-github" href="https://github.com/technophilist/kanbas-react-web-app">Github</a></li>
         </ul>
