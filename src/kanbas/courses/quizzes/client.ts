81897c06372cc16f7fb12064bcf9025375b6b6f5
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-02 13:13:38 -0500

    add .quizzes/client.ts

---
+++ src/kanbas/courses/quizzes/client.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`
+
+const fetchQuizSummariesForCourse = async (courseId: string) => {
+    const response = await axios.get(`${QUIZZES_API}/summary/${courseId}`);
+    return response.data
+}
+
+const updatePublishedStatusOfQuiz = async (quizId: string, isPublished: boolean) => {
+    const response = await axios.patch(`${QUIZZES_API}/${quizId}`, { isPublished })
+    return response.data
+}
+
+const deleteQuiz = async (quizId: string) => {
+    const response = await axios.delete(`${QUIZZES_API}/${quizId}`)
+    return response.data
+}
+
+export { deleteQuiz, fetchQuizSummariesForCourse, updatePublishedStatusOfQuiz }
