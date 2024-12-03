1e028e231331e915a9e535ae4ba12633058fa14c
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-03 09:17:40 -0500

    move DetailsTabContent from QuizDetailsEditorScreen to separate file

---
+++ src/kanbas/courses/quizzes/DetailsTabContent.tsx
@@ -0,0 +1,313 @@
+import { useNavigate } from "react-router-dom"
+import QuizDetail from "./QuizDetail"
+import { useCallback } from "react"
+
+type DetailsTabContentProps = {
+    quiz: QuizDetail,
+    setQuizDetail: (updatedQuizDetail: QuizDetail) => void,
+    onSaveButtonClick: (currentQuiz: QuizDetail) => void
+}
+
+function DetailsTabContent(props: DetailsTabContentProps) {
+    const navigate = useNavigate()
+
+    const getDateFromTimestamp = useCallback((timestamp: string) => {
+        return new Date(parseInt(timestamp)).toISOString().split('T')[0]
+    }, [])
+
+    const getTimestampFromDate = useCallback((date: string) => {
+        return new Date(date).getTime().toString()
+    }, [])
+
+    const handleSave = useCallback(() => {
+        props.onSaveButtonClick(props.quiz)
+        navigate("../")
+    }, [props.quiz])
+    return (
+        <form>
+            <div className="mb-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    value={props.quiz.title}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                        ...props.quiz,
+                        title: e.target.value
+                    })}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label className="form-label text-secondary fw-bold">Quiz Instructions:</label>
+                <textarea className="form-control" rows={3} />
+            </div>
+            <table className="table table-borderless">
+                <tbody>
+                    <tr>
+                        <td className="text-end text-secondary fw-bold" style={{ width: "200px" }}>Quiz Type</td>
+                        <td>
+                            <select
+                                className="form-select"
+                                value={props.quiz.quizType}
+                                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => props.setQuizDetail({
+                                    ...props.quiz,
+                                    quizType: e.target.value
+                                })}
+                            >
+                                <option>Graded Quiz</option>
+                                <option>Practice Quiz</option>
+                                <option>Graded Survey</option>
+                                <option>Ungraded Survey</option>
+                            </select>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td className="text-end text-secondary fw-bold">Assignment Group</td>
+                        <td>
+                            <select
+                                className="form-select"
+                                value={props.quiz.assignmentGroup}
+                                onChange={(e: React.ChangeEvent<HTMLSelectElement>) => props.setQuizDetail({
+                                    ...props.quiz,
+                                    assignmentGroup: e.target.value
+                                })}
+                            >
+                                <option>ASSIGNMENTS</option>
+                                <option>Quizzes</option>
+                                <option>Exams</option>
+                                <option>Projects</option>
+                            </select>
+                            <h6 className="fw-semibold mt-3 mb-3 text-muted">Options</h6>
+                            <div className="d-flex flex-column">
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2"
+                                        id="shuffleAnswers"
+                                        checked={props.quiz.shuffleAnswers}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            shuffleAnswers: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary" htmlFor="shuffleAnswers">Shuffle
+                                        Answers</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2"
+                                        id="showCorrectAnswers"
+                                        checked={props.quiz.showCorrectAnswersImmediately}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            showCorrectAnswersImmediately: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary" htmlFor="showCorrectAnswers">Show
+                                        Correct Answers Immediately</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2"
+                                        id="oneQuestionAtATime"
+                                        checked={props.quiz.oneQuestionAtATime}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            oneQuestionAtATime: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary" htmlFor="oneQuestionAtATime">One
+                                        Question at a Time</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2"
+                                        id="lockQuestions"
+                                        checked={props.quiz.lockQuestionsAfterAnswering}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            lockQuestionsAfterAnswering: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary" htmlFor="lockQuestions">Lock Questions
+                                        After Answering</label>
+                                </div>
+                                <div>
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2"
+                                        id="webcamRequired"
+                                        checked={props.quiz.webcamRequired}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            webcamRequired: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary" htmlFor="webcamRequired">Webcam
+                                        Required</label>
+                                </div>
+                                <div className="d-flex">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input me-2 mb-1 align-self-end"
+                                        id="timeLimit"
+                                    />
+                                    <label className="form-label text-secondary align-self-end m-0" htmlFor="timeLimit">Time
+                                        limit</label>
+                                    <div className="d-flex align-items-center">
+                                        <input
+                                            style={{
+                                                width: "75px"
+                                            }}
+                                            className="form-control ms-5 me-2"
+                                            id="timeLimitMinutes"
+                                            type="number"
+                                            value={props.quiz.timeLimitInMinutes}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                                ...props.quiz,
+                                                timeLimitInMinutes: parseInt(e.target.value)
+                                            })}
+                                        />
+                                        <label className="form-label text-secondary"
+                                            htmlFor="timeLimitMinutes">Minutes</label>
+                                    </div>
+                                </div>
+                                <div
+                                    className="d-flex align-items-center align-content-center p-1 border-top border-bottom border-start border-end rounded-2 mt-4">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input mt-2 ms-2 me-2 align-self-baseline"
+                                        id="multipleAttempts"
+                                        checked={props.quiz.allowMultipleAttempts}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            allowMultipleAttempts: e.target.checked
+                                        })}
+                                    />
+                                    <label className="form-label text-secondary mt-1" htmlFor="multipleAttempts">Allow
+                                        Multiple Attempts</label>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td className="text-end text-secondary fw-bold">Points</td>
+                        <td>
+                            <input
+                                type="number"
+                                className="form-control"
+                                style={{ width: "100px" }}
+                                value={props.quiz.points}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                    ...props.quiz,
+                                    points: parseInt(e.target.value)
+                                })}
+                            />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td className="text-end text-secondary fw-bold">Access Code</td>
+                        <td>
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={props.quiz.accessCode}
+                                onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                    ...props.quiz,
+                                    accessCode: e.target.value
+                                })}
+                            />
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td className="text-end text-secondary fw-bold">Assign</td>
+                        <td>
+                            <div className="border border-1 rounded-1 p-4">
+                                <div className="mb-3">
+                                    <label htmlFor="assignTo" className="form-label text-end text-secondary fw-bold">Assign
+                                        to</label>
+                                    <input
+                                        id="assignTo"
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Assign to"
+                                        value={props.quiz.assignTo}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            assignTo: e.target.value
+                                        })}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="dueDate" className="form-label text-end text-secondary fw-bold">Due
+                                        Date</label>
+                                    <input
+                                        id="dueDate"
+                                        type="date"
+                                        className="form-control"
+                                        value={getDateFromTimestamp(props.quiz.dueDateTimestampMillis)}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                            ...props.quiz,
+                                            dueDateTimestampMillis: getTimestampFromDate(e.target.value)
+                                        })}
+                                    />
+                                </div>
+                                <div className="d-flex gap-3">
+                                    <div className="flex-grow-1">
+                                        <label htmlFor="availableFrom"
+                                            className="form-label text-end text-secondary fw-bold">Available
+                                            from</label>
+                                        <input
+                                            id="availableFrom"
+                                            type="date"
+                                            className="form-control"
+                                            placeholder="Available from"
+                                            value={getDateFromTimestamp(props.quiz.availableFromTimestampMillis)}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                                ...props.quiz,
+                                                availableFromTimestampMillis: getTimestampFromDate(e.target.value)
+                                            })}
+                                        />
+                                    </div>
+                                    <div className="flex-grow-1">
+                                        <label htmlFor="availableUntil"
+                                            className="form-label text-end text-secondary fw-bold">Available
+                                            until</label>
+                                        <input
+                                            id="availableUntil"
+                                            type="date"
+                                            className="form-control"
+                                            placeholder="Until"
+                                            value={getDateFromTimestamp(props.quiz.availableUntilTimestampMillis)}
+                                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => props.setQuizDetail({
+                                                ...props.quiz,
+                                                availableUntilTimestampMillis: getTimestampFromDate(e.target.value)
+                                            })}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className="d-flex justify-content-center gap-2 mt-4">
+                <button type="button" className="btn btn-secondary" onClick={() => navigate("../")}>
+                    Cancel
+                </button>
+                <button type="button" className="btn btn-danger" onClick={handleSave}>
+                    Save
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default DetailsTabContent
