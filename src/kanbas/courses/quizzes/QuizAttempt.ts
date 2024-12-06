6e3ab4bd96363125628573e279dc46881e8acda4
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-05 19:17:41 -0500

    Add quiz attempt type defn

---
+++ src/kanbas/courses/quizzes/QuizAttempt.ts
@@ -0,0 +1,11 @@
+import { AnswerToQuestion } from "./Answer"
+
+type QuizAttempt = {
+    attemptId: string
+    quizId: string
+    uid: string
+    score: number
+    answers: Record<string, AnswerToQuestion>
+}
+
+export default QuizAttempt
