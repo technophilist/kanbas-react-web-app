0210770e7698b78ae091aae7ad43da589989743b
technophilist <54663474+technophilist@users.noreply.github.com>
2024-12-07 13:14:11 -0500

    REMOVE REACT STRICT MODE

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,4 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
-root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+root.render(<App />);
